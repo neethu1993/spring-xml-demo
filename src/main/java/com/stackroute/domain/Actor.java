62be5ce8d45699a9e73302fce47ca4a37e7cbeae
Neethu <neethubabu.babu0@gmail.com>
2019-01-15 17:39:03 +0530

    added constructer injection

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -4,6 +4,15 @@ public class Actor {
     private String name;
     private String gender;
     private int age;
+    public Actor(){
+
+    }
+
+    public Actor(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
 
     public String getName() {
         return name;
